bef9be470c5cf17c8d5c9c3ceff1f70d126ad761
Ritik <varribhar@gmail.com>
2022-07-20 14:03:36 +0530

    product-detail progress

--- client/src/pages/ProductDetail.jsx
+++ client/src/pages/ProductDetail.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const ProductDetail = () => {
   return (
     <div >
-      <div className='bg-white border-solid border-2 border-red lg:w-[1116px]'>
-
+     <div className="m-auto flex justify-center">
+        <div className='bg-white lg:w-[1116px] pt-[16px] shadow-[0_7px_8px_4px_rgba(0,0,0,0.1)] mt-5'>
+          <div className='px-1.5'></div>
+        </div>
       </div>
     </div>
   )
