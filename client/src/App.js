80e9e32aded9ea5c258519fa7a7de5836999d055
Ritik <varribhar@gmail.com>
2022-07-20 13:07:17 +0530

    product box

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,14 @@
 
 function App() {
   return (
-   <div></div>
+    <div >
+      <div className="m-auto flex justify-center">
+        <div className='bg-white lg:w-[1116px] pt-[16px] shadow-[0_7px_8px_4px_rgba(0,0,0,0.1)] mt-5'>
+          this is box
+        </div>
+      </div>
+      
+  </div>
   );
 }
 
---
+++ client/src/pages/ProductDetail.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProductDetail = () => {
+  return (
+    <div >
+      <div className='bg-white border-solid border-2 border-red lg:w-[1116px]'>
+
+      </div>
+    </div>
+  )
+}
+
+export default ProductDetail
