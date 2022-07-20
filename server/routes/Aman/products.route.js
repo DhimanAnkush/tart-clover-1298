4ded7aa24b50e4fbf87ef2c9a03728d001a23cda
Arpit Surana <suranaarpit34@gmail.com>
2022-07-20 20:32:14 +0530

    backend

--- server/routes/Aman/products.route.js
+++ server/routes/Aman/products.route.js
@@ -1,34 +1,31 @@
-const {Router} = require("express");
+const { Router } = require("express");
 const Product = require("../../models/Aman/products.model");
 
+const productRouter = Router();
 
+productRouter.get("/get", async (req, res) => {
+  let data = await Product.find();
+  res.json(data);
+});
 
-const productRouter= Router();
+productRouter.get("/:category", async (req, res) => {
+  if (req.query.sort == "h2l") {
+    var data = await Product.find({ category: req.params.category }).sort({
+      Price: -1,
+    });
+  } else if (req.query.sort == "l2h") {
+    data = await Product.find({ category: req.params.category }).sort({
+      Price: 1,
+    });
+  } else {
+    data = await Product.find({ category: req.params.category });
+  }
+  res.json(data);
+});
 
-productRouter.get("/get",async(req,res)=>{
-    let data= await Product.find();
-    res.json(data);
-})
+productRouter.get("/single/:id", async (req, res) => {
+  let data = await Product.findById({ _id: req.params.id });
+  res.json(data);
+});
 
-productRouter.get("/:category",async (req,res)=>{
-
-    if(req.query.sort=="h2l"){
-      var data= await Product.find({category:req.params.category}).sort({Price:-1});
-    }else if(req.query.sort=='l2h'){
-        data= await Product.find({category:req.params.category}).sort({Price:1});
-    }else{
-        data= await Product.find({category:req.params.category})
-    }  
-    res.json(data)
-})
-
-
-productRouter.get("/single/:id", async (req,res)=>{
- let data= await Product.findById({_id:req.params.id});
- res.json(data)
-})
-
-
-
-
-module.exports=productRouter
+module.exports = productRouter;
