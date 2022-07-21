3b961dcdd2fa5eac22fe50710d771086932c6c0a
Arpit Surana <suranaarpit34@gmail.com>
2022-07-21 14:10:43 +0530

    added npm packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,11 @@
-
+import MainRouter from "./routes/Main.route";
 
 function App() {
   return (
-
-
-   <div className="bg-[#F5F5F5] font-Quicksand"></div>
-
-  
-
+    <div className="bg-[#F5F5F5] font-Quicksand">
+      <MainRouter />
+    </div>
   );
-
 }
 
-export default App
+export default App;
