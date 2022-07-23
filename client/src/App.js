67046529ffdbb978fb32fd48634671ab81ecbf56
Arpit Surana <suranaarpit34@gmail.com>
2022-07-23 12:57:46 +0530

    changes made

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,16 @@
-
-
-
 import { Footer } from "./components/Footer";
 import { Navbar } from "./components/Header";
 
-
 import MainRoutes from "./routes/MainRoutes";
-
-
 
 function App() {
   return (
-
-
-
-   <div className="bg-[#F5F5F5] font-Quicksand h-screen">
-    <Navbar/>
-    <MainRoutes/>
-    <Footer/>
-   </div>
-
-  
-
+    <div className="bg-[#F5F5F5] font-Quicksand h-screen">
+      <Navbar />
+      <MainRoutes />
+      <Footer />
+    </div>
   );
-
 }
 
 export default App;
