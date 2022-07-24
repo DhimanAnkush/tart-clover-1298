27e238ac354039c6fed54ff9add3a917c497a77f
Aman Jaiswal <aman.jaiswal0612@gmail.com>
2022-07-24 10:56:15 +0530

    cartbackend

--- server/routes/P Naga/CartRoute.js
+++ server/routes/P Naga/CartRoute.js
@@ -1,15 +1,22 @@
+
 const { Router } = require('express')
 const Product = require('../../models/Aman/products.model')
 const Cart = require('../../models/P Naga/CartModel')
 
 const route = Router()
 
-route.get('/', async (req, res) => {
-  try {
-    const data = await Cart.find()
-      .pupulate('user', '-password') 
-      .populate('product')
+route.get('/:id', async (req, res) => {
+  console.log(req.params.id, 'iddd')
 
+  if (!req.params.id) {
+    res.status(400).send({ message: 'invalid user' })
+    return
+  }
+
+  try {
+    const data = await Cart.find({ user: req.params.id }).populate('product')
+
+    console.log(data, 'data')
     res.status(200).json(data)
   } catch (e) {
     res.status(400).json(e.message)
@@ -18,7 +25,7 @@ route.get('/', async (req, res) => {
 
 route.post('/', async (req, res) => {
   const data = req.body
-  if (!data) {
+  if (!data.user || !data.product) {
     res.status(400).json({ message: 'Please Provide Valid Inputs' })
     return
   }
@@ -39,11 +46,11 @@ route.put('/:id/increment', async (req, res) => {
   }
 
   try {
-    const response = await Cart.findById(
+    const response = await Cart.findByIdAndUpdate(
       id,
       { $inc: { quantity: 1 } },
       { new: true },
-    )
+    ).populate('product')
     res.status(200).send(response)
   } catch (e) {
     res.status(400).json({ message: 'cant update the field' })
@@ -58,14 +65,29 @@ route.put('/:id/decrement', async (req, res) => {
   }
 
   try {
-    const response = await Cart.findById(
+    const response = await Cart.findByIdAndUpdate(
       id,
       { $inc: { quantity: -1 } },
       { new: true },
-    )
+    ).populate('product')
     res.status(200).send(response)
   } catch (e) {
     res.status(400).json({ message: 'cant update the field' })
+  }
+})
+
+route.delete('/:id', async (req, res) => {
+  const id = req.params.id
+  if (!id) {
+    res.status(400).send({ message: 'id is not valid' })
+    return
+  }
+
+  try {
+    const response = await Cart.deleteOne(req.params)
+    res.status(200).send({ message: 'Deleted' })
+  } catch (e) {
+    res.status(400).send(e.message)
   }
 })
 
