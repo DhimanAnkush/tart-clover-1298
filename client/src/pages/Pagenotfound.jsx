219777e506356cb40cfa84da1350b0f085441443
Arpit Surana <suranaarpit34@gmail.com>
2022-07-25 16:23:04 +0530

    page not found done

---
+++ client/src/pages/Pagenotfound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Pagenotfound = () => {
+  return (
+    <div className="w-fit m-auto">
+      <img src="https://i.stack.imgur.com/6M513.png" alt="404 error" />
+    </div>
+  );
+};
+
+export default Pagenotfound;
--- client/src/routes/MainRoutes.jsx
+++ client/src/routes/MainRoutes.jsx
@@ -1,32 +1,34 @@
-import React from 'react'
-import { Route, Routes } from 'react-router'
-import { Home } from '../components/Home'
-import Brush_Products from '../pages/Brush_Products'
-import Cart from '../pages/Cart/Cart'
-import Checkout from '../pages/Cart/Checkout'
-import Gifting_Products from '../pages/Gifting_Products'
-import Makeup_Products from '../pages/Makeup_Products'
-import ProductDetail from '../pages/ProductDetail'
-import Search from '../pages/Search'
-import Skincare_products from '../pages/skincare_products'
+import React from "react";
+import { Route, Routes } from "react-router";
+import { Home } from "../components/Home";
+import Brush_Products from "../pages/Brush_Products";
+import Cart from "../pages/Cart/Cart";
+import Checkout from "../pages/Cart/Checkout";
+import Gifting_Products from "../pages/Gifting_Products";
+import Makeup_Products from "../pages/Makeup_Products";
+import Pagenotfound from "../pages/Pagenotfound";
+import ProductDetail from "../pages/ProductDetail";
+import Search from "../pages/Search";
+import Skincare_products from "../pages/skincare_products";
 function MainRoutes() {
   return (
     <div>
-        <Routes>
-          <Route path='/' element={<Home/>}></Route>
-            <Route path='/brush-products' element={<Brush_Products/>}/>
-            <Route path='/brush-products/:sub' element={<Brush_Products/>}/>
-            <Route path='/makeup-products' element={<Makeup_Products/>}/>
-            <Route path='/makeup-products/:sub' element={<Makeup_Products/>}/>
-            <Route path= '/skincare-products' element={<Skincare_products/>}/>
-            <Route path= '/gifting-products' element={<Gifting_Products/>}/>
-            <Route path= '/product/:id' element={<ProductDetail/>}/>
-            <Route path= "/search-products/:search" element={<Search/>}/>
-        <Route path='/cart' element={<Cart />} />
-        <Route path="/checkout" element={<Checkout/>}/>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/brush-products" element={<Brush_Products />} />
+        <Route path="/brush-products/:sub" element={<Brush_Products />} />
+        <Route path="/makeup-products" element={<Makeup_Products />} />
+        <Route path="/makeup-products/:sub" element={<Makeup_Products />} />
+        <Route path="/skincare-products" element={<Skincare_products />} />
+        <Route path="/gifting-products" element={<Gifting_Products />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/search-products/:search" element={<Search />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="*" element={<Pagenotfound />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default MainRoutes
+export default MainRoutes;
